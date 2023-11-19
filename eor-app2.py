8ee0c6f00c230920565d468fc4101d76c5673310
Fakhime <86554005+Fakhime@users.noreply.github.com>
2023-11-19 18:29:20 +0300

    Update eor-app2.py

--- eor-app2.py
+++ eor-app2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import Catboost
+import catboost
 from keras.models import load_model
 import matplotlib.pyplot as plt
 import seaborn as sns
